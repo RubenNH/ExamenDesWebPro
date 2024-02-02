877af923d2835147c144af6143897aebd84ffda8
RubenNH <104938082+RubenNH@users.noreply.github.com>
2024-02-02 15:43:49 -0600

    commit uno

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <div class="container fluid">
+      <div class="row">
+        <div class="col-12">
+          <p>
+            Hola a donde quieres ir?
+          </p>
+          <div class="row">
+            <div class="col-6">
+              <router-link to="/registrar" class="btn btn-primary">Registrar vehiculo</router-link>
+            </div>
+            <div class="col-6">
+              <router-link to="/consultar" class="btn btn-primary">consultar</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+
+<script>
+
+</script>
